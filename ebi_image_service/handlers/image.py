97166f60dbf1f0b7f23fb70222801f3403ba72ca
chenjun <chenjun@visionpsn.com>
2018-07-06 15:21:11 +0800

    update api

--- ebi_image_service/handlers/image.py
+++ ebi_image_service/handlers/image.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 from ebi_image_service.logger import config_logger
 from ebi_image_service.utils.driver import WebDriver
@@ -8,14 +8,24 @@ logger = config_logger(__name__, 'info', 'image.log')
 image_bp = Blueprint('image', __name__)
 api = Api(image_bp)
 
+# parser:
+parser = reqparse.RequestParser()
+parser.add_argument(
+    'url',
+    type=str,
+    required=True,
+    location='args'
+)
+
 
 class ImageHandler(Resource):
 
-    def get(self, url):
-        logger.info('[ImageHandler] GET starts, url: %s', url)
+    def get(self):
+        args = parser.parse_args()
+        logger.info('[ImageHandler] GET starts, args: %s', args)
 
         with WebDriver() as driver:
-            driver.get(url)
+            driver.get(args['url'])
 
         return {'success': True}
 
